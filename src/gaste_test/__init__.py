f24fccd7ea1068e1ddc9cd73ffbbf54fdc4ae6ab
wendlina <alexandre.wendling@grenoble-inp.org>
2024-07-26 14:57:52 +0200

    add Table2x2 as import init

--- src/gaste_test/__init__.py
+++ src/gaste_test/__init__.py
@@ -3,4 +3,4 @@ from .combined_pval import (
     get_pval_comb,
     moment_matching_estimator,
 )
-from .stratified_table2x2 import StratifiedTable2x2
+from .stratified_table2x2 import StratifiedTable2x2, Table2x2
